38ae3110b72068724a640a97afa43a45655d2953
Archana Panchal <panchalarchana12@gmail.com>
2019-08-29 19:58:49 +0530

    [Archana,Ashima] Refactored Probability functionlity.

--- src/main/com/tw/vapasi/EventProbability.java
+++ src/main/com/tw/vapasi/EventProbability.java
@@ -2,19 +2,23 @@ package com.tw.vapasi;
 
 // chances of occurence of two events which could be similar.
 public class EventProbability {
-    private double eventOneProbability;
-    private double eventTwoProbability;
+    private double coinProbability;
+    private double diceProbability;
 
-    public EventProbability(double eventProbability1, double EventProbability2) {
-        this.eventOneProbability = eventProbability1;
-        this.eventTwoProbability = EventProbability2;
+
+    public EventProbability(double coinProbability,double diceProbability) {
+        this.coinProbability = coinProbability;
+        this.diceProbability = diceProbability;
     }
 
-    public boolean CompareProbability() {
-        return eventOneProbability == eventTwoProbability;
+    public boolean compareProbability() {
+        return coinProbability == diceProbability;
     }
-
+    double combinedProbability(){
+        return coinProbability * diceProbability;
+    }
     public boolean equals(Object obj) {
+
         if ((obj != null) && obj instanceof EventProbability) {
             return true;
         }
--- src/test/com/tw/vapasi/EventProbabilityTest.java
+++ src/test/com/tw/vapasi/EventProbabilityTest.java
@@ -8,35 +8,45 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class EventProbabilityTest {
     @Test
     void expectEqualProbability() {
-        EventProbability probability = new EventProbability(0.50,0.50);
-        assertEquals(true, probability.CompareProbability());
+        EventProbability probability = new EventProbability(0.50, 0.50);
+        assertEquals(true, probability.compareProbability());
     }
+
     @Test
     void expectNotEqualProbability() {
-        EventProbability probability = new EventProbability(0.50,0.16);
-        assertNotEquals(true, probability.CompareProbability());
+        EventProbability probability = new EventProbability(0.50, 0.16);
+        assertNotEquals(true, probability.compareProbability());
     }
+
     @Test
     void expectEqualProbabilityBetweenSameReferences() {
-        EventProbability probabilityOne = new EventProbability(0.50,0.16);
+        EventProbability probabilityOne = new EventProbability(0.50, 0.16);
         assertEquals(true, probabilityOne.equals(probabilityOne));
     }
+
     @Test
     void expectFalseWhenReferenceisNull() {
-        EventProbability probabilityOne = new EventProbability(0.50,0.16);
+        EventProbability probabilityOne = new EventProbability(0.50, 0.16);
         assertEquals(false, probabilityOne.equals(null));
     }
+
     @Test
     void expectNotEqualProbabilityBetweenTwoReferences() {
-        EventProbability probabilityOne = new EventProbability(0.50,0.16);
-        EventProbability probabilityTwo = new EventProbability(0.25,0.10);
+        EventProbability probabilityOne = new EventProbability(0.50, 0.16);
+        EventProbability probabilityTwo = new EventProbability(0.25, 0.10);
         assertEquals(false, probabilityOne.equals(probabilityTwo));
     }
+
     @Test
     void expectEqualProbabilityBetweenTwoReferences() {
-        EventProbability probabilityOne = new EventProbability(0.50,0.16);
-        EventProbability probabilityTwo = new EventProbability(0.50,0.50);
+        EventProbability probabilityOne = new EventProbability(0.50, 0.16);
+        EventProbability probabilityTwo = new EventProbability(0.50, 0.50);
         assertEquals(true, probabilityTwo.equals(probabilityTwo));
     }
 
+    @Test
+    void expectCombinedProbabilityoftwoEvents() {
+        EventProbability combinedprobability = new EventProbability(0.50, 0.50);
+        assertEquals(0.25, combinedprobability.combinedProbability());
+    }
 }
